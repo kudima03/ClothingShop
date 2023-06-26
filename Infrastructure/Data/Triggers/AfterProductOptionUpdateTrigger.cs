c3d862af2ba0d87ced1184374c38e08a229fbfbb
Dmitry Kurochkin <93078951+kudima03@users.noreply.github.com>
2023-06-26 12:35:32 +0300

    Real time product quantity changes integrated.

---
+++ Infrastructure/Data/Triggers/AfterProductOptionUpdateTrigger.cs
@@ -0,0 +1,35 @@
+﻿using ApplicationCore.Entities;
+using DomainServices.Features.ProductOptions.Notifications;
+using EntityFrameworkCore.Triggered;
+using MediatR;
+
+namespace Infrastructure.Data.Triggers;
+public class ProductOptionAfterUpdateTrigger : IAfterSaveTrigger<ProductOption>
+{
+    private readonly IMediator _mediator;
+
+    public ProductOptionAfterUpdateTrigger(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public Task AfterSave(ITriggerContext<ProductOption> context, CancellationToken cancellationToken)
+    {
+        if (context.ChangeType != ChangeType.Modified)
+        {
+            return Task.CompletedTask;
+        }
+
+        long productOptionId = context.Entity.Id;
+        long productId = context.Entity.ProductId;
+        int newQuantity = context.Entity.Quantity;
+        int? oldQuantity = context.UnmodifiedEntity?.Quantity;
+        if (oldQuantity != newQuantity)
+        {
+            return _mediator.Publish(new ProductOptionQuantityChangedNotification(productOptionId, productId, newQuantity),
+                cancellationToken);
+        }
+
+        return Task.CompletedTask;
+    }
+}
---
+++ Web/Services/Interfaces/ISubscribesToProductsManager.cs
@@ -0,0 +1,14 @@
+﻿namespace Web.Services.Interfaces;
+
+public interface ISubscribesToProductsManager
+{
+    void AddSubscribe(long userId, long productId, string connectionId);
+
+    void RemoveSubscribe(string connectionId);
+
+    IEnumerable<string> ConnectionsSubscribedToProduct(long productId);
+
+    int SubscribedToProductUsersCount(long productId);
+
+    long ProductConnectionSubscribedTo(string connectionId);
+}
